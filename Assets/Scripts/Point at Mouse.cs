d7b72420bba1b10c322e44170fb0db713069bfb9
Leo Schonberger (Windows Desktop) <leoschonberger@gmai.com>
2021-02-26 09:48:44 -0800

    Added pointAtMouse script for arm

---
+++ Assets/Scripts/Point at Mouse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //rotation
+        Vector3 mousePos = Input.mousePosition; // Sets a vector variable "mosePos" = to the mouse position.
+        mousePos.z = 5.23f; //Distance between camera and object/scene.
+ 
+        Vector3 objectPos = Camera.main.WorldToScreenPoint (transform.position);//Finds object position
+        mousePos.x = mousePos.x - objectPos.x;//Point left/right comparing to objectPos
+        mousePos.y = mousePos.y - objectPos.y;//Point up/down
+ 
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;//Calculate angle of pointing between 
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));//Sets rotation based on angle
+    }
+}

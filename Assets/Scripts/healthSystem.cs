63141ffaf8009d8ce89bdb41410365ab94c648de
Leo Schonberger (Windows Desktop) <leoschonberger@gmai.com>
2021-03-02 09:57:45 -0800

    Added hurtbox coliders

---
+++ Assets/Scripts/healthSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthSystem : MonoBehaviour
+{
+	    public int startingHealth = 100;
+		public int warningHealth = 10;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	
+}

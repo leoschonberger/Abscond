81a86307767c0addddca8be945a674daa66c141d
Tucky Helm <tucky.helm@gmail.com>
2021-03-12 09:56:47 -0800

    Buttons are now functional

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void playGame()
     {
-        
+        SceneManager.LoadScene("MainScene");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Settings()
     {
         
     }

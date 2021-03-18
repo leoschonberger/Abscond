d7da368468c55bce94607baa81c4ee4592a0a75b
Tucky Helm <tucky.helm@gmail.com>
2021-03-17 20:44:45 -0700

    Updated the main menu
    There is now a background image, title text, and filler text for the settings screen. All of the images and formatting most likely will be replaced.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,23 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    
-    void Start()
-    {
-        //QuitButton.gameObject.SetActive(false);    
-    }
     public void playGame()
     {
         SceneManager.LoadScene("MainScene");
     }
-
     public void QuitGame()
     {
         Application.Quit();
-    }
-
-    public void Settings()
-    {
-        
     }
 }

93deac3bb949420196ad3f5519f3378b03c879d8
Tucky Helm <tucky.helm@gmail.com>
2021-04-06 10:01:04 -0700

    Main Menu Fix
    "Level 1 Tiled" now starts when play button is pressed.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ public class MainMenu : MonoBehaviour
 {
     public void playGame()
     {
-        SceneManager.LoadScene("MainScene");
+        SceneManager.LoadScene("Level 1 Tiled");
     }
     public void QuitGame()
     {

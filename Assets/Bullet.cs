a9583fcbbf8412e5e3295422a3327b0b34f2bf73
Tucky Helm <tucky.helm@gmail.com>
2021-04-12 09:59:07 -0700

    Bullets!
    Bullets shoot when mouse is clicked. In the future, this will be changed so that bullets shoot every 2 seconds or so.

---
+++ Assets/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float bulletSpeed = 20f;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb.velocity = transform.right * bulletSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}

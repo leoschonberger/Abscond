ad587bf9e5c2970742077ff314328d01e40649d0
Ben Bridenbaugh <benbridenbaugh@gmail.com>
2021-05-20 14:21:03 -0700

    Working on respawn

--- Assets/Characters/Scripts/CollisionDetection.cs
+++ Assets/Characters/Scripts/CollisionDetection.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using Characters.Scripts.PhysicsCode;
 using UnityEngine;
 
@@ -10,50 +11,59 @@ namespace Characters.Scripts
 
         public Rigidbody2D MainRigidbody2D;
         public Transform MainTransform;
+
+        public Vector3 respawnLocation;
         
         private Vector2 previousVelocity;
+
+        private void Start()
+        {
+            respawnLocation = gameObject.GetComponentInParent<Transform>().position;
+            //throw new NotImplementedException();
+        }
+
         protected virtual void OnTriggerEnter2D(Collider2D collision)
         {
-            if (collision.gameObject.layer == 14)
+            
+            switch (collision.gameObject.layer)
             {
-                return;
-            }
-            if (collision.gameObject.layer == 20) //teleporter
-            {
+                case 12: //Death Pit
+                    enemyPhysics.velocity = Vector2.zero;
+                    break;
+                case 14: //Room Trigger
+                    return;
+                //teleporter
+                case 20: 
+                {
+                    GameObject teleporter; //more efficient this way
+                    var teleporterScript = (teleporter = collision.gameObject).GetComponent<Teleporter>();
                 
-                GameObject teleporter; //more efficient this way
-                var teleporterScript = (teleporter = collision.gameObject).GetComponent<Teleporter>();
+                    MainRigidbody2D.position = teleporterScript.teleportDestination.position;
+                    var cooldown = TeleporterCooldown(teleporterScript.cooldownLength, teleporter, teleporterScript.secondTeleporter);
+                    StartCoroutine(cooldown); //Makes it so that the enemy wont fall on to one of the teleporters and get stuck in an infinite loop
                 
-                MainRigidbody2D.position = teleporterScript.teleportDestination.position;
-                var cooldown = TeleporterCooldown(teleporterScript.cooldownLength, teleporter, teleporterScript.secondTeleporter);
-                StartCoroutine(cooldown); //Makes it so that the enemy wont fall on to one of the teleporters and get stuck in an infinite loop
-                
-                return;
-            }
+                    return;
+                }
+                default:
+                    enemyPhysics.EnterBulletTime();
 
-            enemyPhysics.EnterBulletTime();
-
-                //Debug.Log(collision.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+                    //Debug.Log(collision.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+                    break;
+            }
         }
 
         private void OnTriggerExit2D(Collider2D other)
         {
-            if (other.gameObject.layer == 14)
+            switch (other.gameObject.layer)
             {
-                return;
+                case 12:
+                case 14:
+                case 20:
+                    return;
+                default:
+                    enemyPhysics.ExitBulletTime();
+                    break;
             }
-
-            if (other.gameObject.layer == 20)
-            {
-                //MainRigidbody2D.WakeUp();
-                //enemyPhysics.velocity = previousVelocity;
-                //Debug.Log(enemyPhysics.velocity);
-                return;
-            }
-            
-            enemyPhysics.ExitBulletTime();
-            //Debug.Log("this actually works");
-            //throw new NotImplementedException();
         }
 
         private IEnumerator TeleporterCooldown(float cooldownTime, GameObject teleporter1, GameObject teleporter2)
---
+++ Assets/Scripts/DeathPitRespawn.cs
@@ -0,0 +1,20 @@
+﻿using Characters.Scripts;
+using UnityEngine;
+
+public class DeathPitRespawn : MonoBehaviour
+{
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log("hello");
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.GetComponentInParent<Transform>().position = other.gameObject.GetComponent<CollisionDetection>().respawnLocation;
+        }
+        
+        //other.gameObject.transform.position = other.gameObject.GetComponent<PlayerController>().respawnLocation;
+        //other.gameObject.transform.position = other.gameObject.GetComponentInChildren<PlayerController>().respawnLocation;
+        //throw new NotImplementedException();
+    }
+}

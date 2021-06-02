8f6ecf5dcb8c38e9ae5746ddc7a7982e69c43021
Tucky Helm <tucky.helm@gmail.com>
2021-06-02 10:02:01 -0700

    Teleporter
    Added teleporter prefab which takes player from level to level. Also added tilemap 2D collider

---
+++ Assets/LevelTeleport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTeleport : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform playerTransform;
+    public Transform teleporter;
+    public String nextLevel;
+    void Start()
+    {
+        teleporter = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTransform.position.x > teleporter.position.x)
+        {
+            SceneManager.LoadScene(nextLevel);
+        }
+    }
+}
